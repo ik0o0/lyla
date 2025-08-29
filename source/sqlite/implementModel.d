c15455c72f40b27244bfcca0e00ee7a046ec7a33
ik0o0 <162647005+ik0o0@users.noreply.github.com>
2025-08-29 10:46:06 +0200

    Added the open source header to sqlite/implementModel.d.

--- source/sqlite/implementModel.d
+++ source/sqlite/implementModel.d
@@ -1,3 +1,18 @@
+/*
+ * This file is part of a project licensed under the GNU Lesser General Public License v3 (LGPLv3).
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU LGPLv3 as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this project.  If not, see <https://www.gnu.org/licenses/>.
+ */
 module sqlite.implementModel;
 
 import std.stdio;
